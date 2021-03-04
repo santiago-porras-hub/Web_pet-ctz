8c799aac07986175f7901f0ddd73912761b133c2
santiago porras <sporrass@unbosque.edu.co>
2021-03-04 16:46:58 -0500

    d3 library

---
+++ js/load-csv.js
@@ -0,0 +1,14 @@
+var allData;
+d3.dsv( ";", "../data/pets.citizens.csv" )
+  .then( function( data ) {
+
+  allData= data;
+printData();
+  } )
+  .catch( function( error ) {
+  
+  } );
+
+  function printData(data){
+  	console.log(data);
+  }
