7d75a652b6eed3eff18fb18e3db6adb175c94f2c
santiago porras <sporrass@unbosque.edu.co>
2021-03-17 21:56:39 -0500

    Upload css and methods

--- js/load-csv.js
+++ js/load-csv.js
@@ -3,6 +3,7 @@ d3.dsv( ";", "../data/pets-citizens.csv" )
 .then( function( data ) {
 
 	allData= data;
+	
 	printData();
 	edit();
 	addNewPet();
@@ -11,25 +12,42 @@ d3.dsv( ";", "../data/pets-citizens.csv" )
 
 } );
 
+var dataPoints = [];
+
+for (var prop in allData) {
+    var arr = [];
+    for(key in allData[prop]){
+        arr.push(allData[prop][key]);
+    }
+    dataPoints.push(arr);
+}   
+
 
 function edit(){
 	
 	console.log(allData[0] );
-	for (var i = 0; i < allData.length; i++) {
-		
+	
 	console.log(allData[allData.length-1]);
-	var microchip = allData[i]["microchip"]=document.getElementById('upmicro').value;
-	var species=  allData[i]["species"]=document.getElementById('upspecie').value;
-	var sex =allData[i]["sex"]=document.getElementById('upsex').value;
-	var size =allData[i]["size"]=document.getElementById('upsize').value;
-	var potent=allData[i]["potentDangerous"]=document.getElementById('uppotent').value;
-	var neigh=allData[i]["neighborhood"]=document.getElementById('upneighboorhood').value;
 
-	allData.push({"microchip": microchip,"species": species,"sex": sex,"size": size,"potentDangerous":potent,"neighborhood":neigh,"race": document.getElementById('uprace').value});
-	console.log(allData[0] );
-	}
+	var microchip = document.getElementById('upmicro').value;
 	
 
+	for (var i = 0; i < allData.length; i++) {
+
+		if( microchip= allData[i].microchip){
+
+			var species=  allData[i]["species"]=document.getElementById('upspecie').value;
+			var sex =allData[i]["sex"]=document.getElementById('upsex').value;
+			var size =allData[i]["size"]=document.getElementById('upsize').value;
+			var potent=allData[i]["potentDangerous"]=document.getElementById('uppotent').value;
+			var neigh=allData[i]["neighborhood"]=document.getElementById('upneighboorhood').value;
+
+			allData.push({"microchip": microchip,"species": species,"sex": sex,"size": size,"potentDangerous":potent,"neighborhood":neigh,"race": document.getElementById('uprace').value});
+			console.log(allData[0] );
+			break;
+		}
+
+	}
 	
 }
 
@@ -38,44 +56,34 @@ function addNewPet(){
 	allData.push({"microchip" : document.getElementById('Microchip').value, "species" : document.getElementById('Specie').value, "sex" : document.getElementById('Sex').value, "size" :document.getElementById('Size').value, "potentDangerous" : document.getElementById('Potent').value, "neighborhood" : document.getElementById('Neighborhood').value});	
 }
 
-function printData(data){
+$(document).ready(function() {
+    $('#example').DataTable( {
+        data: dataPoints,
+        columns: [
+            { title: "Microchip" },
+            { title: "Position" },
+            { title: "Office" },
+            { title: "Extn." },
+            { title: "Start date" },
+            { title: "Salary" }
+        ]
+    } );
+} );
 
-	for(var i = 1; i < table.rows.length; i++)
-	{
-
-		for(var j = 0; j < table.rows[i].cells.length; j++)
-		{
-			table.rows[i].cells[0].innerHTML = allData[i-1]["microchip"];
-		}
-
-		for(var j = 0; j < table.rows[i].cells.length; j++)
-		{
-			table.rows[i].cells[1].innerHTML = allData[i-1]["species"];
-		}
-
-		for(var j = 0; j < table.rows[i].cells.length; j++)
-		{
-			table.rows[i].cells[2].innerHTML = allData[i-1]["sex"];
-		}
-
-		for(var j = 0; j < table.rows[i].cells.length; j++)
-		{
-			table.rows[i].cells[3].innerHTML = allData[i-1]["size"];
-		}
-
-		for(var j = 0; j < table.rows[i].cells.length; j++)
-		{
-			table.rows[i].cells[4].innerHTML = allData[i-1]["potentDangerous"];
-		}
-
-		for(var j = 0; j < table.rows[i].cells.length; j++)
-		{
-			table.rows[i].cells[5].innerHTML = allData[i-1]["neighborhood"];
-		}
-	}
-
-
+function mostrar(){
+  var archivo = document.getElementById("file").files[0];
+  var reader = new FileReader();
+  if (file) {
+    reader.readAsDataURL(archivo );
+    reader.onloadend = function () {
+      document.getElementById("img").src = reader.result;
+    }
+  }
 }
+
+
+
+
 
 
 
