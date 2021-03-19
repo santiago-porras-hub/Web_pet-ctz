60892ef70ccc687418cbc948510498a1888c06ac
santiago porras <sporrass@unbosque.edu.co>
2021-03-18 21:15:56 -0500

    Pagination and edit

--- js/load-csv.js
+++ js/load-csv.js
@@ -7,10 +7,55 @@ d3.dsv( ";", "../data/pets-citizens.csv" )
 	printData();
 	edit();
 	addNewPet();
+	mostrar();
 } )
 .catch( function( error ) {
 
 } );
+
+function changePage(page)
+{
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var listing_table = document.getElementById("listingTable");
+    var page_span = document.getElementById("page");
+
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    listing_table.innerHTML = "";
+
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
+        listing_table.innerHTML += allData[i].microchip +"&nbsp"+ allData[i].species + "&nbsp"+ allData[i].size+"&nbsp"+ allData[i].sex+"&nbsp" +allData[i].potentDangerous+"&nbsp"+ allData[i].neighborhood+"<br>";
+    }
+    page_span.innerHTML = page;
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+}
+
+function numPages()
+{
+    return Math.ceil(allData.length / records_per_page);
+}
+
+window.onload = function() {
+    changePage(1);
+};
+
+
+
+
 
 var dataPoints = [];
 
@@ -21,7 +66,6 @@ for (var prop in allData) {
     }
     dataPoints.push(arr);
 }   
-
 
 function edit(){
 	
@@ -53,22 +97,10 @@ function edit(){
 
 function addNewPet(){
 
-	allData.push({"microchip" : document.getElementById('Microchip').value, "species" : document.getElementById('Specie').value, "sex" : document.getElementById('Sex').value, "size" :document.getElementById('Size').value, "potentDangerous" : document.getElementById('Potent').value, "neighborhood" : document.getElementById('Neighborhood').value});	
+	allData.unshift({"microchip" : document.getElementById('Microchip').value, "species" : document.getElementById('Specie').value, "sex" : document.getElementById('Sex').value, "size" :document.getElementById('Size').value, "potentDangerous" : document.getElementById('Potent').value, "neighborhood" : document.getElementById('Neighborhood').value});	
+console.log(allData[0]);
 }
 
-$(document).ready(function() {
-    $('#example').DataTable( {
-        data: dataPoints,
-        columns: [
-            { title: "Microchip" },
-            { title: "Position" },
-            { title: "Office" },
-            { title: "Extn." },
-            { title: "Start date" },
-            { title: "Salary" }
-        ]
-    } );
-} );
 
 function mostrar(){
   var archivo = document.getElementById("file").files[0];
@@ -81,14 +113,24 @@ function mostrar(){
   }
 }
 
+var current_page = 1;
+var records_per_page = 5;
 
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
 
-
-
-
-
-
-
+function nextPage()
+{
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+}
 
 
 
